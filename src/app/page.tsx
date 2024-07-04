930950f519c6b8ea7c1e20793d3a0f55a243ce38
Shivang Sharma <sharmashivang1999@gmail.com>
2024-07-04 12:00:11 +0530

    style: added css to navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center">Melody Hub</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl text-center text-white">Melody Hub</h1>
+    </main>
   );
 }
