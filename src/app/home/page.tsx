c70ef62492c9f0342859260d8047d801b6816632
Shivang Sharma <sharmashivang1999@gmail.com>
2024-07-04 11:33:20 +0530

    feat: injected navbar

--- src/app/home/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-    return (
-        <h1 className="text-3xl mt-2 text-center"> Home Page </h1>
-    )
-}
--- src/app/home/ui/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Ui() {
-  return <h1 className="text-2xl mt-2 text-center">UI</h1>;
-}
